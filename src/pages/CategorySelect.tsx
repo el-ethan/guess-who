8fcb1a156bf7319405e95dc34434568f182f55ae
Ethan Skinner <10249884+el-ethan@users.noreply.github.com>
2026-02-22 16:21:37 -0800

    Restricted categories

--- src/pages/CategorySelect.tsx
+++ src/pages/CategorySelect.tsx
@@ -7,15 +7,28 @@ interface Props {
 export default function CategorySelect({ onSelect }: Props) {
   const modules = getAllImages()
   const categories = getCategories(modules)
+  
 
   return (
     <div className="menu">
       <h1>Guess Who</h1>
-      {categories.map(c => (
-        <button key={c} onClick={() => onSelect(c)}>
-          {c.replace(/-/g, ' ')}
-        </button>
-      ))}
+      {categories.map(c => {
+        const label = c.replace(/_restricted$/, '').replace(/-/g, ' ')
+        const handleClick = () => {
+          if (c.endsWith('_restricted')) {
+            const ok = confirm('this category may contain scary images. Do you want to continue')
+            if (ok) onSelect(c)
+          } else {
+            onSelect(c)
+          }
+        }
+
+        return (
+          <button key={c} onClick={handleClick}>
+            {label}
+          </button>
+        )
+      })}
     </div>
   )
 }
