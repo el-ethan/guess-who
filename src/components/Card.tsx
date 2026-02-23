9b080a7f0a24ca81cb195e32e72216f859dd3e88
Ethan Skinner <10249884+el-ethan@users.noreply.github.com>
2026-02-23 10:02:15 -0800

    Updates

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,14 +6,16 @@ interface Props {
 }
 
 export default function Card({ image, label, flipped, onClick }: Props) {
+  const displayLabel = label.split('-')[0]
   return (
     <div className={`card ${flipped ? 'flipped' : ''}`} onClick={onClick}>
       <div className="card-inner">
         <div className="card-front">
           <img src={image} />
-          <span>{label}</span>
         </div>
-        <div className="card-back" />
+        <div className="card-back">
+          <span>{displayLabel}</span>
+        </div>
       </div>
     </div>
   )
